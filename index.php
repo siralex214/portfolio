7ff38c19b0b399e77d53fe091adbf17d8bc27534
alexis mouchon <alexismouchon09@gmail.com>
2022-11-18 21:24:30 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+$css = [
+    "./assets/css/reset.css",
+    "./assets/css/style.css",
+];
+$js = [
+    "./assets/js/main.js",
+]
+?>
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Alexis Mouchon | Portfolio</title>
+    <?php foreach ($css as $value): ?>
+        <link rel="stylesheet" href="<?= $value ?>">
+    <?php endforeach; ?>
+    <?php foreach ($js as $value): ?>
+        <script src="<?= $value ?>" defer></script>
+    <?php endforeach; ?>
+</head>
+<body>
+<header></header>
+<main>
+    <p>Work in progress!</p>
+</main>
+<footer></footer>
+</body>
+</html>
