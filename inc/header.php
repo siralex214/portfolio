9fe921ff394d7f93dbd35d43cca06dc29672dbdd
alexis mouchon <alexismouchon09@gmail.com>
2022-11-24 14:25:08 +0100

    add header and footer file

--- inc/header.php
+++ inc/header.php
@@ -1,1 +1,21 @@
 <?php
+?>
+
+<header>
+    <section id="bandeau-superieur">
+        <div class="wrapper-bandeau-superieur">
+            <div id="container-information-bandeau-superieur">
+                <a href="tel:+33777785521">+33 7 77 78 55 21</a>
+                <a href="mailto:contact@alexis-mouchon.fr">contact@alexis-mouchon.fr</a>
+            </div>
+            <div>
+                <a href="mailto:contact@alexis-mouchon.fr">
+                    <button id="button-devis">Besoin d'un devis gratuit?</button>
+                </a>
+            </div>
+        </div>
+    </section>
+    <section id="bandeau-inferieur">
+
+    </section>
+</header>
