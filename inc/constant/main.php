8ea072494034e2091c0838dca1cff0726686f4ef
alexis mouchon <alexismouchon09@gmail.com>
2022-11-24 14:27:03 +0100

    add constant

---
+++ inc/constant/main.php
@@ -0,0 +1,15 @@
+<?php
+$constant = [
+    "SITE_NAME" => "Alexis Mouchon développeur web | ",
+    "SITE_URL" => "https://www.alexis-mouchon.fr/",
+    "ROUTE" => [
+        "home" => [
+            "name" => "Accueil",
+            "url" => "./index.php",
+        ],
+        "404" => [
+            "name" => "404",
+            "url" => "./404.php",
+        ],
+    ],
+];
