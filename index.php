87b073902a7639302ca63b638aa2cdcfc80ded5e
alexis mouchon <alexismouchon09@gmail.com>
2022-11-25 17:13:53 +0100

    push

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ $meta_description = "Bienvenue sur le site d'Alexis Mouchon, développeur web en
         <div id="container-welcome">
             <h1 id="title">Besoin d'aide pour développer <br> votre activité sur le web?</h1>
             <h2 id="subtitle">Vous êtes au bon endroit.</h2>
-            <a href="./">
+            <a href="/">
                 <button id="button-formule">Voir les formules</button>
             </a>
         </div>
@@ -35,6 +35,35 @@ $meta_description = "Bienvenue sur le site d'Alexis Mouchon, développeur web en
     <section id="second-part">
         <h3>Développeur web FullStack <br> sur Rouen depuis 2022</h3>
         <h4>Création de site web, site vitrine, site e-commerce <br> et site sur-mesure</h4>
+    </section>
+    <section id="third-part">
+        <div class="container-card">
+            <div class="card">
+                <h3>Qui suis-je?</h3>
+                <p>Je m’appelle Alexis Mouchon, développeur web en freelance sur Rouen.</p>
+                <br>
+                <p>Je met à disposition mes compétences pour développer vos projets:</p>
+                <p>- sites vitrine</p>
+                <p>- e-commerce</p>
+                <p>- site sur mesure en fonction de vos besoins.</p>
+                <br>
+                <p>Vrai passionné par tout ce qui touche au développement web, c’est avec plaisir que je met à
+                    disposition mes talents pour vous.</p>
+            </div>
+            <div class="card">
+                <h3>Pourquoi faire appel à mes services?</h3>
+                <p>J’ai actuellement 2 ans d’expériences dans le développement web. Je continue tout les jours à me
+                    former pour être à jour des dernières technologies.</p>
+                <br>
+                <p>Je réalise vos projets web avec soin et professionnalisme pour assurer un service de qualité.</p>
+                <br>
+                <p>Je suis toujours à la recherche de nouveaux projets. N’hésiitez pas à me contacter pour avoir plus
+                    d’information.</p>
+            </div>
+        </div>
+    </section>
+    <section id="fourth-part">
+
     </section>
 </main>
 <?php include_once "./inc/footer.php" ?>
